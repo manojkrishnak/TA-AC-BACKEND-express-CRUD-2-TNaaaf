98326b3f011f6865681cf57cdd911aba97f8750b
manojkrishnak <manojkrishna11@gmail.com>
2022-02-19 08:33:45 +0530

    add files:mongoose:block-BNaadl

--- block-BNaadl/app.js
+++ block-BNaadl/app.js
@@ -1,1 +1,28 @@
-console.log(value)
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+const articleRouter = require("./router/article");
+
+//connect to mongo db 
+mongoose.connect("mongodb://localhost/articles", err => {
+    console.log("connected ", err ? false : true);
+});
+
+// view engine
+app.set("view engine", "ejs");
+app.set("views", path.join("views", ));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// home route
+app.get("/", (req, res) => {
+    res.send("Welcome to Blog");
+});
+
+// articles routes
+app.use("/article", articleRouter);
+
+
+app.listen(4444, () => console.log("connected to 4444"));
